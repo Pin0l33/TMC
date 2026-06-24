4205a40432a9f0b8e9aced04622c3f6be51846c6
Pin0l33 <delpinol553@gmail.com>
2026-06-24 19:49:15 -0300

    Update registro.php

--- Web TMC/TmC_web/Web/registro.php
+++ Web TMC/TmC_web/Web/registro.php
@@ -2,32 +2,16 @@
 
 include("conexion.php");
 
-$username = $_POST['usuario'];
+$usuario = $_POST['usuario'];
 $email = $_POST['email'];
-$password_hash = password_hash(
-    $_POST['contraseña'],
-    PASSWORD_DEFAULT
-);
+$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO USUARIO
-(
-    username,
-    email,
-    password_hash
-)
-VALUES
-(
-    '$username',
-    '$email',
-    '$password_hash'
-)";
+$sql = "INSERT INTO usuarios (usuario, email, contraseña)
+        VALUES ('$usuario', '$email', '$contraseña')";
 
-if(mysqli_query($conn, $sql))
-{
+if (mysqli_query($conn, $sql)) {
     echo "Usuario registrado correctamente";
-}
-else
-{
+} else {
     echo "Error: " . mysqli_error($conn);
 }
 
